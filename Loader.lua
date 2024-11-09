3605564c218a9b6675ac7c46fb01aa5ff6745c92
setthreadidentity <joshuabond2006@gmail.com>
2024-11-08 21:26:33 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,51 +1,26 @@
 --// Nyx.lua | Loader | Developer: Adulting. \\--
 local script_details = {
-    debug = false, 
-    version = "",
-    url = "https://raw.githubusercontent.com/setthreadidentity/Nyx.lua/refs/heads/main/Imports.lua"
+    debug = false, -- Set to false for live GitHub link
+    version = "1.0.2",
 }
 
+local url = "https://raw.githubusercontent.com/setthreadidentity/Nyx.lua/main"
+
+local out = script_details.debug and function(T, ...)
+    return warn("[Nyx - DEBUG]: " .. T:format(...))
+end or function() end
+
 local function import(file)
-    if script_details.debug then
-        print("[Nyx Debug]: Importing File \"%s\"", file)
-    end
-    local success, result = pcall(function()
-        return loadstring(game:HttpGet(script_details.url .. file))()
+    out("Importing File \"%s\"", file)
+    local x, a = pcall(function()
+        return loadstring(game:HttpGet(url .. file))()
     end)
-    if not success then
-        if script_details.debug then
-            warn("[Nyx Debug]: Failed to import", file)
-        end
-        return  
-    end
-    return result  
-end
-
-local CheckVersion = true
-
-local function checkVersion()
-    if CheckVersion then
-        local success, result = pcall(function()
-            return loadstring(game:HttpGet(script_details.url .. "/Version.lua"))()
-        end)
-        if not success then
-            warn("Failed to fetch version information.")
-            return false
-        end
-        if result ~= script_details.version then
-            game.Players.LocalPlayer:Kick("[Nyx Debug]: Version Mismatch.")
-            return false
-        end
-        return true
-    else
-        return
+    if not x then
+        warn('Failed to import', file, ':', a)
     end
 end
-
 
 getgenv().import = import
-getgenv().script_details = script_details
+getgenv().details = script_details
 
-if checkVersion() then
-    import('/Imports.lua')
-end
+import("/Imports.lua")
